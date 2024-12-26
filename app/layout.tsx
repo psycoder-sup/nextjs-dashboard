0e9599acc41fdb92e4ac6423b9aedfe4dd23ddfd
SangUk Park <parksang1993@gmail.com>
2024-12-26 13:04:44 -0800

    Beginning of chapter 6

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { font } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
